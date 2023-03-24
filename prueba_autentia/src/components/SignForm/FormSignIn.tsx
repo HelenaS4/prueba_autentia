ae07b9abd18428081bca914364ec4352964cb070
HelenaS4 <ssanchez1523@gmail.com>
2023-03-24 18:53:28 +0100

    Subiendo HomePage y SignPage

---
+++ prueba_autentia/src/components/SignForm/FormSignIn.tsx
@@ -0,0 +1,5 @@
+const FormSignIn = () => {
+
+}
+
+export default FormSignIn
---
+++ prueba_autentia/src/pages/SignInPage.tsx
@@ -0,0 +1,11 @@
+const SignInPage = () => {
+    return (
+        <>
+            <h1>Holap</h1>
+         // TODO: Formulario inicio sesión
+
+        </>
+    )
+}
+
+export default SignInPage
