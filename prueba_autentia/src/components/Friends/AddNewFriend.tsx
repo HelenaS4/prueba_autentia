cf8b5338b1808482dde37e061f2ce38055cf4f60
HelenaS4 <ssanchez1523@gmail.com>
2023-03-27 22:10:11 +0200

    Cambios

--- prueba_autentia/src/components/Friends/AddNewFriend.tsx
+++ prueba_autentia/src/components/Friends/AddNewFriend.tsx
@@ -1,14 +1,43 @@
+import { redirect } from "react-router-dom";
+import "./AddNewFriendStyle.css"
+
+const friends = JSON.parse(localStorage.getItem('friends') || '{}');
+const friends_group = JSON.parse(localStorage.getItem('friends_group') || '{}');
+const session_user = JSON.parse(localStorage.getItem('user_data') || '{}');
+
+const addNewFriend = (friend:any) => {
+    let friend_already_exists = friends_group.data.find((friend_group:any) => friend_group.id == friend.id)
+
+    console.log(friend_already_exists);
+    
+    if (!friend_already_exists) {
+        friends_group.data.push(friend);
+        localStorage.setItem('friends_group', JSON.stringify(friends_group));
+        window.location.reload();
+    }
+}
+
 const AddNewFriend = () => {
-    const friends = JSON.parse(localStorage.getItem('friends') || '{}');
-    const friends_group = JSON.parse(localStorage.getItem('friends_group') || '{}')
+    const friends_without_me:any = [];
+
+    for (const friend of friends.data) {
+        if (friend.uuid != session_user.uuid) {
+            friends_without_me.push(friend)
+        }
+    }
 
     return (
         <>
             <h1>Ugh people</h1>
-
-            {friends.data.map((friend:any) => (
-                <li key={friend.id}>{friend.name} <button >+</button></li>
-            ))}
+            <ul>
+                {friends_without_me.map((friend:any) => (
+                    <li key={friend.id} className="friend-item">
+                        <span>{friend.name}</span>
+                        <button className="plus-button" onClick={() => addNewFriend(friend)}>+</button>
+                    </li>
+                ))}
+            </ul>
+            
         </>
     )
 }
--- prueba_autentia/src/pages/Expenses/NewExpensePage.tsx
+++
@@ -1,9 +0,0 @@
-const NewExpensePage = () => {
-    return (
-        <>
-            <h1>New Friend!</h1>
-        </>
-    )
-}
-
-export default NewExpensePage
